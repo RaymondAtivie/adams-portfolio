bf6c8b36da12e69c0dbc149f27daa95cfa6cd50f
Dorathy <dorathymaryok@gmail.com>
2021-02-24 16:31:43 +0100

    Projects page editing

--- public/navbar.php
+++ public/navbar.php
@@ -1,10 +1,10 @@
-<nav class="flex justify-between items-center py-14 text-white font-about">
+<nav class="flex items-center justify-between text-white py-14 font-about">
     <div class="">
         <a href="index.php"><img class=""src="img/portfolio.png" alt="David Adams Logo"></a>
     </div>
     <div class="">
         <a href="about.php" class="pl-8 font-medium">About</a>
-        <!-- <a href="projects.php" class="pl-8 font-medium">Projects</a> -->
+        <a href="projects.php" class="pl-8 font-medium">Projects</a>
         <a href="adamsdavid-cv.pdf" class="pl-8 font-medium">Resume</a>
         <a href="contact.php" class="pl-8 font-medium">Contact</a>
     </div>
